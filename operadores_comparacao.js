c11ccd4e46cdea050c5fcdf465023ed8a4beef48
Giovanni <giovanni.agasperini@gmail.com>
2025-04-09 22:01:38 -0300

    módulo completo

---
+++ operadores_comparacao.js
@@ -0,0 +1,24 @@
+
+
+// Igual ==
+// console.log(valor1 == valor2);
+
+// Estritamente Igual === (Tanto tipo quanto valor)
+//console.log(valor1 === valor2)
+
+// Diferente !=
+//console.log(valor1 != valor2)
+
+//Estritamente Diferente !==
+//console.log(valor1 !== valor2)
+
+let valor1 = 5;
+let valor2 = 20;
+
+// Maior > | Maior ou Igual >=
+//console.log(valor1 > valor2);
+//console.log(valor1 >= valor2);
+
+// Menor < | Menor ou igual <=
+console.log(valor1 < valor2);
+console.log(valor1 <= valor2);
---
+++ operadores_logicos.js
@@ -0,0 +1,17 @@
+// Operador Lógico AND && (afirmação)
+const freteGratis = false;
+const estoque = true;
+
+//console.log(freteGratis && estoque);
+
+// Operador Lógico OU ||
+//console.log(freteGratis || estoque);
+
+// Operador Lógico NOT (negação !)
+
+//console.log(!freteGratis);
+
+// Dupla negação !! Usada para converter um valor para seu equivalente booleano
+const nome = "Matheus";
+console.log(!!nome);
+
